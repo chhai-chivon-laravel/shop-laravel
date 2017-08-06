5a463bfaf9b06f6e5944dc7f0f0151d27059589b
chhaichivon <chivonchhai@hotmail.com>
2017-08-06 21:56:00 +0700

    shop-laravel

---
+++ resources/views/products/form.blade.php
@@ -0,0 +1,18 @@
+{{--Created by PhpStorm.
+    User: ChhaiChivon
+	Date: 8/6/2017
+    Time: 8:11 PM
+--}}
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Form</title>
+</head>
+<body>
+		Form Create
+</body>
+</html>
---
+++ resources/views/products/list.blade.php
@@ -0,0 +1,21 @@
+
+
+{{--Created by PhpStorm.
+    User: ChhaiChivon
+	Date: 8/6/2017
+    Time: 8:11 PM
+--}}
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>list</title>
+</head>
+<body>
+		Hello World TO ME
+</body>
+</html>
